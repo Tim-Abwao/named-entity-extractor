2a104069e50cffbb5533f6fb84fba60d9ad631be
Tim-Abwao <abwaomusungu@gmail.com>
2020-05-26 23:09:59 +0300

    Updating code and requirements

--- text-mining.py
+++ text-mining.py
@@ -5,7 +5,7 @@ import pandas as pd
 import spacy
 from tika import unpack
 
-cwd = os.getcwd()  # getting current working directory
+cwd = os.getcwd() 
 # Tk GUI to select document to process
 root = Tk()
 root.filename = filedialog.askopenfilename(initialdir=cwd,
@@ -25,35 +25,37 @@ print('\n\n\t', '*'*3, ' Successfully extracted text ', '*'*3, '\n\n', sep='')
 print('-'*50, '\nProcessing Text With SpaCy...\n', '-'*50, sep='')
 nlp = spacy.load("en_core_web_sm")
 print('\nSpaCy model loaded.\n\nObtaining Named Entities...')
-doc = nlp(text)  # applying sPacy NLP pipeline
+doc = nlp(text)  
 
 # Getting named-entity information
 ent_text = []
-ent_label = []
-ent_sentence = []
+ent_labels = []
+ent_sentences = []
 for entity in doc.ents:
     ent_text.append(entity.text)  # the entity's text
-    ent_label.append(entity.label_)  # the type of entity (PERSON, ORG, etc)
+    ent_labels.append(entity.label_)  # the type of entity (PERSON, ORG, etc)
     # Getting some context on the obtained named-entity
-    ent_sentence.append(entity.sent.text.replace('\n', ' '))
-# Getting brief descriptions of each entity type
-ent_description = [spacy.explain(_label) for _label in ent_label]
+    ent_sentences.append(entity.sent.text.replace('\n', ' '))
 
 print('\n\t', '*** Extracted {} entities ***\n'.format(len(doc.ents)), sep='')
 print('Saving results to excel file...\n\n')
 
-# Combining obtained information
-df = pd.DataFrame({'Entity': ent_text, 'Type': ent_label,
-                   'Description': ent_description, 'Context': ent_sentence})
+
+text_data = pd.DataFrame({'Entity': ent_text, 'Type': ent_labels, 
+                          'Context': ent_sentences})
+labels = text_data.Type.unique()
+ent_descriptions = [spacy.explain(_label) for _label in labels]
+Descriptions = pd.DataFrame({"Type": labels, "Description": ent_descriptions})
 
 # Tk GUI to select destination directory
 save_dir = filedialog.askdirectory(title='Please select destination folder')
 output_file = save_dir + '/text-mining.xlsx'
 
-# Exporting the entity-info into an excel file, with a sheet for each type:
-entities = list(df.groupby('Type'))  # a list of (type, dataframe) tuples.
-with pd.ExcelWriter(output_file) as writer:
-    for ent_name, data in entities:
-        data.to_excel(writer, sheet_name=ent_name, index=False)
+# Exporting the entity-info to an excel file (a sheet for each type)
+with pd.ExcelWriter('text-mining.xlsx') as writer:
+    Descriptions.to_excel(writer, sheet_name="DEFINITIONS", index=False)
+    for ent_name, data in text_data.groupby('Type'):
+        data[["Entity","Context"]].to_excel(writer, sheet_name=ent_name, 
+                                            index=False)
 
-print('\t', '*'*3, ' Done! ', '*'*3, '\n\nSaved file: ' + output_file, sep='')
+print('\t', '***', ' Done! ', '***', '\n\nSaved file: ' + output_file, sep='')
