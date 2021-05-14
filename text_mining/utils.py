be57910a41db0c63df1faf5fd85312ef05e5e067
Tim-Abwao <abwaomusungu@gmail.com>
2021-05-14 17:48:42 +0300

    Update utils.py
    Remove redundant functions. Give functions and variables intuitive names. Update docstrings.

--- text_mining/utils.py
+++ text_mining/utils.py
@@ -1,138 +1,152 @@
 #!/usr/bin/env python3
-from tkinter.filedialog import askopenfilename, asksaveasfilename
-import pandas as pd
+import logging
 from pathlib import Path
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+import pandas as pd
 import spacy
 import textract
-import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s"
+)
 
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s: %(message)s')
+def get_input_file():
+    """Create a file-dialog to navigate to and select a file.
 
-
-def select_file():
-    """Create a file dialog to help navigate to and select a file.
-
-    Returns:
-        `pathlib.Path`: The path to the file.
+    Returns
+    -------
+    The path to the file.
     """
-    # select file
     file = askopenfilename(
-        initialdir='.', title='Please select a document',
-        filetypes=(('pdf', '*.pdf'),
-                   ('text', '*.txt'),
-                   ('word', '*.docx'),
-                   ('all files', '*.*'))
-        )
+        initialdir=".",
+        title="Please select a document",
+        filetypes=(
+            ("pdf", "*.pdf"),
+            ("text", "*.txt"),
+            ("word", "*.docx"),
+            ("all files", "*.*"),
+        ),
+    )
     if not file:  # If file dialog is closed with no file selected
         return None
     else:
-        logging.info(f'Found file {file!r}')
+        logging.info(f"Found file {file!r}")
         return Path(file)
 
 
-def get_text_from_file():
-    """Read and parse the contents of a file as a string.
+def read_text_from_file():
+    """Get the contents of a file as a string.
 
-    Returns:
-        str : The file's contents as one continuous string
+    Returns
+    -------
+    The file's contents as one continuous string.
     """
-    filepath = select_file()
+    filepath = get_input_file()
 
-    try:
-        text = textract.process(filepath)
-    except TypeError:
+    if filepath is None:
         return None
+    else:
+        try:
+            # Get the file's contents as bytes
+            text = textract.process(filepath)
+        except UnicodeDecodeError:
+            return None
 
-    return text.decode()
+    return text.decode()  # Convert bytes to string
 
 
 def extract_entity_info(text):
-    """Get information on the named entities present in the text.
+    """Get the named entities present in the text.
 
-    Args:
-        text (str): The text to process
+    Parameters
+    ----------
+    text : str
+        The text to process.
 
-    Returns:
-        `pandas.DataFrame`: A dataframe with 3 columns - 'Entity', 'Type' and
-        'Context'.
+    Returns
+    -------
+    A dictionary of entity predictions and descriptions.
     """
-    # Load the pretrained spaCy model
-    logging.info('Analysing extracted text...')
-    nlp = spacy.load('en_core_web_md')
+    # Load the pre-trained spaCy model
+    logging.info("Analysing extracted text...")
+    nlp = spacy.load("en_core_web_md")
 
     # Get entity predictions
     doc = nlp(text)
-    entity_info = ((entity.text, entity.label_, entity.sent.text)
-                   for entity in doc.ents)
+    entity_info = (
+        (entity.text, entity.label_, entity.sent.text) for entity in doc.ents
+    )
+    entity_predictions = (
+        pd.DataFrame(entity_info, columns=["Entity", "Type", "Context"])
+        # Remove 'newline' to get continuous text
+        .applymap(lambda x: x.replace("\n", " "))
+    )
 
-    entity_df = pd.DataFrame(entity_info,
-                             columns=['Entity', 'Type', 'Context'])
-    logging.info(f'Done. {len(entity_df)} named entities found.')
+    logging.info(f"Done. {len(entity_predictions):,} named entities found.")
 
-    # Remove 'newline' to get continuous text
-    return entity_df.applymap(lambda x: x.replace('\n', ' '))
+    # Get entity-type descriptions
+    entity_types = entity_predictions["Type"].unique()
+    entity_descriptions = pd.DataFrame(
+        {"Type": entity_types, "Description": map(spacy.explain, entity_types)}
+    )
+    return {
+        "predictions": entity_predictions,
+        "descriptions": entity_descriptions,
+    }
 
 
-def get_entity_descriptions(entity_df):
-    """Get a dataframe of entity descriptions.
+def set_output_file():
+    """Create a file dialog to select a destination for the results.
 
-    Args:
-        entity_df (`pandas.DataFrame`): A dataframe of entity information with
-        3 columns - 'Entity', 'Type' and 'Context'.
-
-    Returns:
-        `pandas.DataFrame`: A pandas dataframe with entity types and their
-        respective descriptions.
+    Returns
+    -------
+    The path to the output-file.
     """
-
-    entity_types = entity_df['Type'].unique()
-    return pd.DataFrame(
-        {'Type': entity_types,
-         'Description': map(spacy.explain, entity_types)})
-
-
-def select_file_destination():
-    """Create a file dialog to help select a destination filename.
-
-    Returns:
-        `pathlib.Path`: The path to a file.
-    """
-    # Get desired output-file-name
     output_file = asksaveasfilename(
-        initialdir='.', initialfile='text_results.xlsx',
-        filetypes=[('Excel', '.xlsx')]
+        initialdir=".",
+        initialfile="text_results.xlsx",
+        filetypes=[("Excel", ".xlsx")],
     )
     if not output_file:  # If file dialog is closed with no file selected
-        return Path('entity-info.xlsx')
+        return Path("entity-info.xlsx")
     else:
         return Path(output_file)
 
 
-def save_results_to_excel(entity_df):
+def save_results_to_excel(entity_info):
     """Save extracted information as an excel file, with a sheet for each
     entity type.
 
-    Args:
-        entity_df (`pandas.DataFrame`): A dataframe of entity information with
-        3 columns - 'Entity', 'Type' and 'Context'.
-    Returns:
-        str : The path to the created file.
+    Parameters
+    ----------
+    entity_info : dict
+        A dictionary with entity predictions and descriptions.
+
+    Returns
+    -------
+    The path to the output file.
     """
     # Get destination filename
-    output_file = select_file_destination()
+    output_file = set_output_file()
 
+    entity_predictions = entity_info["predictions"]
+    entity_descriptions = entity_info["descriptions"]
+
+    # Write entity info to an excel file
     with pd.ExcelWriter(output_file) as writer:
-        # Get entity descriptions & write them as the first sheet
-        entity_descriptions = get_entity_descriptions(entity_df)
-        entity_descriptions.to_excel(writer, index=False,
-                                     sheet_name='DEFINITIONS')
-        # Create a sheet for each entity type
-        for ent_name, ent_data in entity_df.groupby('Type'):
-            (ent_data[['Entity', 'Context']]
+        # Write entity descriptions in the first sheet
+        entity_descriptions.to_excel(
+            writer, index=False, sheet_name="DEFINITIONS"
+        )
+        # Create a sheet for each entity predicted type
+        for ent_name, ent_data in entity_predictions.groupby("Type"):
+            (
+                ent_data[["Entity", "Context"]]
                 .dropna()
-                .to_excel(writer, index=False, sheet_name=ent_name))
+                .to_excel(writer, index=False, sheet_name=ent_name)
+            )
 
-    logging.info(f'Results saved as "{output_file}"')
+    logging.info(f"Results saved as '{output_file}'")
     return output_file
