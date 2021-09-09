757d6309f722d18035ac17b1c7b639909a1305e5
Tim-Abwao <abwaomusungu@gmail.com>
2021-09-09 23:17:42 +0300

    Update utility functions
    Process text in chunks of 100000 characters to avoid memory allocation errors.
    Update docstrings and add type annotations.
    Improve readability and reduce cruft.

--- entity_extractor/utils.py
+++ entity_extractor/utils.py
@@ -2,24 +2,28 @@
 import logging
 from pathlib import Path
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from typing import Dict, Optional
 
 import pandas as pd
 import spacy
 import textract
+from spacy.lang import en
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s"
 )
+CHUNK_SIZE = 100000
 
 
-def get_input_file():
+def get_input_file() -> Optional[Path]:
     """Create a file-dialog to navigate to and select a file.
 
     Returns
     -------
-    The path to the file.
+    Path
+        Input file.
     """
-    file = askopenfilename(
+    filename = askopenfilename(
         initialdir=".",
         title="Please select a document",
         filetypes=(
@@ -29,124 +33,141 @@ def get_input_file():
             ("all files", "*.*"),
         ),
     )
-    if not file:  # If file dialog is closed with no file selected
-        return None
-    else:
-        logging.info(f"Found file {file!r}")
-        return Path(file)
+
+    return Path(filename) if filename else None
 
 
-def read_text_from_file():
+def read_text_from_file() -> Optional[str]:
     """Get the contents of a file as a string.
 
     Returns
     -------
-    The file's contents as one continuous string.
+    Optional[str]
+        A file's contents as one continuous string.
     """
     filepath = get_input_file()
 
     if filepath is None:
         return None
     else:
-        try:
-            # Get the file's contents as bytes
-            text = textract.process(filepath)
-        except UnicodeDecodeError:
-            return None
-
-    return text.decode()  # Convert bytes to string
+        logging.info(f"Found '{filepath}'. Extracting text...")
+        return textract.process(filepath).decode()
 
 
-def extract_entity_info(text):
+def process_text_chunk(nlp: en.English, text_chunk: str) -> pd.DataFrame:
+    """Get named entity info in a portion of the text not more than
+    `CHUNK_SIZE` characters long. Chunking is necessary to avoid memory
+    allocation errors.
+
+    Parameters
+    ----------
+    nlp : spacy.lang.en.English
+        Pretrained model/pipeline
+    text_chunk : str
+        The text to process
+
+    Returns
+    -------
+    pd.DataFrame
+        Entity name, type and context as columns.
+    """
+    doc = nlp(text_chunk)
+    entity_info = (
+        (entity.text, entity.label_, entity.sent.text) for entity in doc.ents
+    )
+    return pd.DataFrame(
+        entity_info, columns=["Entity", "Type", "Context"]
+    ).drop_duplicates()
+
+
+def extract_entity_info(text: str) -> Dict[str, pd.DataFrame]:
     """Get the named entities present in the text.
 
     Parameters
     ----------
     text : str
-        The text to process.
+        The text to process
 
     Returns
     -------
-    A dictionary of entity predictions and descriptions.
+    Dict[str, pd.DataFrame]
+        Named entity info and named entity type descriptions.
     """
-    # Load the pre-trained spaCy model
-    logging.info("Analysing extracted text...")
+    logging.info("Predicting named entities...")
+
     nlp = spacy.load("en_core_web_md")
-
-    # Get entity predictions
-    doc = nlp(text)
-    entity_info = (
-        (entity.text, entity.label_, entity.sent.text) for entity in doc.ents
+    text_chunks = (
+        text[idx: idx + CHUNK_SIZE] for idx in range(0, len(text), CHUNK_SIZE)
     )
-    entity_predictions = (
-        pd.DataFrame(entity_info, columns=["Entity", "Type", "Context"])
-        # Remove 'newline' to get continuous text
-        .applymap(lambda x: x.replace("\n", " "))
+    entity_data_list = [
+        process_text_chunk(nlp, chunk) for chunk in text_chunks
+    ]
+    entity_df = (
+        pd.concat(entity_data_list, ignore_index=True)
+        .drop_duplicates()
+        .applymap(lambda txt: txt.replace("\n", " "))
     )
 
-    logging.info(f"Done. {len(entity_predictions):,} named entities found.")
+    logging.info(f"Done. {len(entity_df):,} named entities found.")
 
-    # Get entity-type descriptions
-    entity_types = entity_predictions["Type"].unique()
+    entity_types = entity_df["Type"].unique()
     entity_descriptions = pd.DataFrame(
         {"Type": entity_types, "Description": map(spacy.explain, entity_types)}
     )
     return {
-        "predictions": entity_predictions,
+        "predictions": entity_df,
         "descriptions": entity_descriptions,
     }
 
 
-def set_output_file():
+def set_output_file() -> Path:
     """Create a file dialog to select a destination for the results.
 
     Returns
     -------
-    The path to the output-file.
+    Path
+        The desired output file's path.
     """
     output_file = asksaveasfilename(
         initialdir=".",
         initialfile="text_results.xlsx",
         filetypes=[("Excel", ".xlsx")],
     )
-    if not output_file:  # If file dialog is closed with no file selected
-        return Path("entity-info.xlsx")
-    else:
-        return Path(output_file)
+
+    return Path(output_file) if output_file else Path("entity-info.xlsx")
 
 
-def save_results_to_excel(entity_info):
+def save_results_to_excel(entity_info: Dict[str, pd.DataFrame]) -> Path:
     """Save extracted information as an excel file, with a sheet for each
     entity type.
 
     Parameters
     ----------
-    entity_info : dict
-        A dictionary with entity predictions and descriptions.
+    entity_info : Dict[str, pd.DataFrame]
+        Named entity predictions and type descriptions.
 
     Returns
     -------
     The path to the output file.
     """
-    # Get destination filename
     output_file = set_output_file()
 
     entity_predictions = entity_info["predictions"]
     entity_descriptions = entity_info["descriptions"]
 
-    # Write entity info to an excel file
     with pd.ExcelWriter(output_file) as writer:
         # Write entity descriptions in the first sheet
         entity_descriptions.to_excel(
             writer, index=False, sheet_name="DEFINITIONS"
         )
-        # Create a sheet for each entity predicted type
+        # Create a sheet for each entity type
         for ent_name, ent_data in entity_predictions.groupby("Type"):
             (
                 ent_data[["Entity", "Context"]]
                 .dropna()
+                .sort_values(by="Entity")
                 .to_excel(writer, index=False, sheet_name=ent_name)
             )
 
-    logging.info(f"Results saved as '{output_file}'")
+    logging.info(f"Results saved as '{output_file}'\n")
     return output_file
